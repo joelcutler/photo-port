e5a4fdcea2ac8430b1f2d933c5baf7363f4b49b9
Joel C <joel7921111@yahoo.com>
2022-06-01 19:56:17 -0600

    tests for contact

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -4,13 +4,15 @@ import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Nav from "..";
 
+afterEach(cleanup);
+
 const categories = [
   { name: "portraits", description: "Portraits of people in my life" },
 ];
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
-
-afterEach(cleanup);
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 describe("Nav component", () => {
   it("renders", () => {
@@ -19,6 +21,8 @@ describe("Nav component", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
       />
     );
   });
@@ -29,6 +33,8 @@ describe("Nav component", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
       />
     );
 
@@ -43,6 +49,8 @@ describe("emoji is visible", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
       />
     );
 
@@ -57,6 +65,8 @@ describe("links are visible", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
       />
     );
 
